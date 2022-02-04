8435232e6ed825b6d3c261be2861302de39d6715
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-02-04 17:01:54 +0100

    Ticket

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,5 @@
-@foreach ($movies as $movie)
+@section('pageContent')
+    @foreach ($movies as $movie)
     <div class="card">
         <span class="card__original-title">{{$movie['original_title']}}</span>
         <span class="card__title">{{$movie->title}}</span>
@@ -6,4 +7,5 @@
         <span class="card__date">{{$movie->date}}</span>
         <span class="card__vote">{{$movie['vote']}}</span>
     </div>
-@endforeach
+    @endforeach
+@endsection
